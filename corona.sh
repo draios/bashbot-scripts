8cad27d612900bff1fce1a12ce5e70bba4813f16
mathew-fleisch <mathew.fleisch@gmail.com>
2020-07-23 08:33:42 -0700

    Update messaging in corona script

--- corona.sh
+++ corona.sh
@@ -34,7 +34,7 @@ while IFS= read -r county; do
   tmp_data=',{"county":"'$this_county'","infected":'$this_totalcountconfirmed',"population":'$this_population',"percentage":"'$this_percentage'"}'$tmp_data
 done <<< "$data"
 better_data=$(echo "$tmp_data" | sed -e 's/^,//g' | sed -e 's/^\(.*\)$/[\1]/g' | jq -c '. | sort_by(.'$SORT_BY') | '$SORT_ORDER'[]')
-
+echo "Californians coronavirus infected by county: $TARGET_DATE (sort: $SORT_BY)"
 echo "╭─────────────────┬────────────┬─────────────┬──────────────╮"
 echo "│     County      │  Infected  │  Population │   %Infected  │"
 echo "├─────────────────┼────────────┼─────────────┼──────────────┤"
